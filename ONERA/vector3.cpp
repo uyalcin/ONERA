93b1512fbe98ada559ff7bb55ba3673aa415b893
Ufuk Yalcin <yalcin.ufuk59@gmail.com>
2018-03-09 11:54:19 +0100

    Vector3

--- ONERA/vector3.cpp
+++ ONERA/vector3.cpp
@@ -1,7 +1,36 @@
 #include "vector3.h"
 
-Vector3::Vector3()
+Vector3::Vector3() :
+    x(0),
+    y(0),
+    z(0)
+{
+}
+
+Vector3::Vector3(float _x, float _y, float _z) :
+    x(_x),
+    y(_y),
+    z(_z)
+{
+}
+
+Vector3::~Vector3()
 {
 
+}
+
+float Vector3::getX()
+{
+    return x;
+}
+
+float Vector3::getY()
+{
+    return y;
+}
+
+float Vector3::getZ()
+{
+    return z;
 }
 
--- ONERA/vector3.h
+++ ONERA/vector3.h
@@ -4,8 +4,19 @@
 
 class Vector3
 {
-public:
-    Vector3();
+    public:
+        Vector3();
+        Vector3(float _x, float _y, float _z);
+        ~Vector3();
+
+        float getX();
+        float getY();
+        float getZ();
+
+    private:
+        float x;
+        float y;
+        float z;
 };
 
 #endif // VECTOR3_H
